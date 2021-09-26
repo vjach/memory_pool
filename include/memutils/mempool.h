b414b802f57449709f1b4d664fa84b86edbbc781
Victor-Stefan Jach <14238510+vjach@users.noreply.github.com>
2021-09-26 18:44:37 +0200

    Google code-style

--- include/memutils/mempool.h
+++ include/memutils/mempool.h
@@ -7,60 +7,60 @@ namespace memutils {
 
 template <size_t blocks, size_t block_size>
 class MemoryPool {
-   private:
-    struct Deleter {
-        Deleter(MemoryPool& pool) : pool{pool} {}
-        void operator()(void* address) {
-            if (address != nullptr) {
-                auto ptr = reinterpret_cast<uint8_t*>(address);
-                auto block_addr =
-                    reinterpret_cast<BlockHeader*>(ptr - sizeof(BlockHeader));
-                block_addr->next = pool.free_block_;
-                pool.free_block_ = block_addr;
-            }
-        }
-
-        MemoryPool& pool;
-    };
-
-   public:
-    using UniqueMemoryPtr = std::unique_ptr<void, Deleter>;
-    MemoryPool() : free_block_{nullptr}, untouched_blocks_{blocks} {}
-
-    auto Allocate() {
-        if (free_block_) {
-            auto block = free_block_;
-            free_block_ = free_block_->next;
-            return UniqueMemoryPtr{reinterpret_cast<void*>(block->data),
-                                   Deleter(*this)};
-        }
-
-        if (untouched_blocks_) {
-            --untouched_blocks_;
-            auto block = reinterpret_cast<BlockHeader*>(
-                arena_ + total_block_size * untouched_blocks_);
-            return UniqueMemoryPtr{reinterpret_cast<void*>(block->data),
-                                   Deleter(*this)};
-        }
-
-        return UniqueMemoryPtr{nullptr, Deleter(*this)};
+ private:
+  struct Deleter {
+    Deleter(MemoryPool& pool) : pool{pool} {}
+    void operator()(void* address) {
+      if (address != nullptr) {
+        auto ptr = reinterpret_cast<uint8_t*>(address);
+        auto block_addr =
+            reinterpret_cast<BlockHeader*>(ptr - sizeof(BlockHeader));
+        block_addr->next = pool.free_block_;
+        pool.free_block_ = block_addr;
+      }
     }
 
-    constexpr const uint16_t Size() const { return blocks; }
-    constexpr const uint16_t BlockSize() const { return block_size; }
+    MemoryPool& pool;
+  };
 
-   private:
-    struct BlockHeader {
-        struct BlockHeader* next;
-        uint8_t data[0];
-    } __attribute__((packed));
+ public:
+  using UniqueMemoryPtr = std::unique_ptr<void, Deleter>;
+  MemoryPool() : free_block_{nullptr}, untouched_blocks_{blocks} {}
 
-    static constexpr const size_t total_block_size =
-        block_size + sizeof(BlockHeader);
-    uint8_t arena_[total_block_size * blocks];
+  auto Allocate() {
+    if (free_block_) {
+      auto block = free_block_;
+      free_block_ = free_block_->next;
+      return UniqueMemoryPtr{reinterpret_cast<void*>(block->data),
+                             Deleter(*this)};
+    }
 
-    BlockHeader* free_block_;
-    size_t untouched_blocks_;
+    if (untouched_blocks_) {
+      --untouched_blocks_;
+      auto block = reinterpret_cast<BlockHeader*>(
+          arena_ + total_block_size * untouched_blocks_);
+      return UniqueMemoryPtr{reinterpret_cast<void*>(block->data),
+                             Deleter(*this)};
+    }
+
+    return UniqueMemoryPtr{nullptr, Deleter(*this)};
+  }
+
+  constexpr const uint16_t Size() const { return blocks; }
+  constexpr const uint16_t BlockSize() const { return block_size; }
+
+ private:
+  struct BlockHeader {
+    struct BlockHeader* next;
+    uint8_t data[0];
+  } __attribute__((packed));
+
+  static constexpr const size_t total_block_size =
+      block_size + sizeof(BlockHeader);
+  uint8_t arena_[total_block_size * blocks];
+
+  BlockHeader* free_block_;
+  size_t untouched_blocks_;
 };
 
 }  // namespace memutils
--- tests/mempool-tests.cc
+++ tests/mempool-tests.cc
@@ -9,54 +9,54 @@ using Pool = MemoryPool<16, 1024>;
 
 TEST(MempoolTest, CanCreateIntsance) { MemoryPool<16, 1024> pool; }
 TEST(MempoolTest, CanAllocate) {
-    Pool pool;
-    {
-        auto chunk = pool.Allocate();
-        EXPECT_TRUE(chunk != nullptr);
-    }
+  Pool pool;
+  {
+    auto chunk = pool.Allocate();
+    EXPECT_TRUE(chunk != nullptr);
+  }
 }
 
 TEST(MempoolTest, AllocateAll) {
-    Pool pool;
-    auto exhaust_pool = [&pool]() {
-        std::queue<Pool::UniqueMemoryPtr> queue;
-        for (auto i = 0; i < pool.Size(); ++i) {
-            auto chunk = pool.Allocate();
-            EXPECT_TRUE(chunk != nullptr);
-            queue.push(std::move(chunk));
-        }
+  Pool pool;
+  auto exhaust_pool = [&pool]() {
+    std::queue<Pool::UniqueMemoryPtr> queue;
+    for (auto i = 0; i < pool.Size(); ++i) {
+      auto chunk = pool.Allocate();
+      EXPECT_TRUE(chunk != nullptr);
+      queue.push(std::move(chunk));
+    }
 
-        EXPECT_TRUE(pool.Allocate() == nullptr);
-    };
+    EXPECT_TRUE(pool.Allocate() == nullptr);
+  };
 
-    exhaust_pool();
-    exhaust_pool();
+  exhaust_pool();
+  exhaust_pool();
 }
 
 TEST(MempoolTest, NoDataCorruption) {
-    Pool pool;
-    std::queue<Pool::UniqueMemoryPtr> queue;
-    for (auto i = 0; i < pool.Size(); ++i) {
-        auto chunk = pool.Allocate();
-        EXPECT_TRUE(chunk != nullptr);
-        memset(chunk.get(), (uint8_t)i, pool.BlockSize());
-        queue.push(std::move(chunk));
+  Pool pool;
+  std::queue<Pool::UniqueMemoryPtr> queue;
+  for (auto i = 0; i < pool.Size(); ++i) {
+    auto chunk = pool.Allocate();
+    EXPECT_TRUE(chunk != nullptr);
+    memset(chunk.get(), (uint8_t)i, pool.BlockSize());
+    queue.push(std::move(chunk));
+  }
+
+  auto is_all = [](uint8_t* ptr, uint8_t value, unsigned size) -> bool {
+    bool ret = true;
+    for (unsigned i = 0; i < size; ++i) {
+      ret = ret and (ptr[i] == value);
     }
 
-    auto is_all = [](uint8_t* ptr, uint8_t value, unsigned size) -> bool {
-        bool ret = true;
-        for (unsigned i = 0; i < size; ++i) {
-            ret = ret and (ptr[i] == value);
-        }
+    return ret;
+  };
 
-        return ret;
-    };
-
-    for (auto i = 0; i < pool.Size(); ++i) {
-        auto chunk = std::move(queue.front());
-        queue.pop();
-        EXPECT_TRUE(is_all(reinterpret_cast<uint8_t*>(chunk.get()), i,
-                           pool.BlockSize()));
-    }
+  for (auto i = 0; i < pool.Size(); ++i) {
+    auto chunk = std::move(queue.front());
+    queue.pop();
+    EXPECT_TRUE(
+        is_all(reinterpret_cast<uint8_t*>(chunk.get()), i, pool.BlockSize()));
+  }
 }
 
