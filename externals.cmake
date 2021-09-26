a5cb50c6a57bc0d513926adff6aa5151464db454
Victor-Stefan Jach <14238510+vjach@users.noreply.github.com>
2021-09-26 19:16:53 +0200

    ignore googletest when CC

--- externals.cmake
+++ externals.cmake
@@ -1,12 +1,14 @@
 include(FetchContent)
-FetchContent_Declare(
-    google_test
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    SOURCE_DIR ${PROJECT_SOURCE_DIR}/externals/gtest
-    )
+if (NOT CMAKE_CROSSCOMPILING)
+    FetchContent_Declare(
+        google_test
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        SOURCE_DIR ${PROJECT_SOURCE_DIR}/externals/gtest
+        )
 
-if(NOT google_test_POPULATED)
-    FetchContent_Populate(google_test)
-    add_subdirectory(${google_test_SOURCE_DIR} ${google_test_BINARY_DIR})
+    if(NOT google_test_POPULATED)
+        FetchContent_Populate(google_test)
+        add_subdirectory(${google_test_SOURCE_DIR} ${google_test_BINARY_DIR})
+    endif()
 endif()
 
